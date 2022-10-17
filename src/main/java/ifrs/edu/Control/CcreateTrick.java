9348bceeb25a14256b08c5ed5b608e7e197e0edb
rob <rob@rob.com>
2022-10-17 20:53:41 -0300

    validTrick removed

--- src/main/java/ifrs/edu/Control/CcreateTrick.java
+++ src/main/java/ifrs/edu/Control/CcreateTrick.java
@@ -6,17 +6,7 @@ import ifrs.edu.Entity.Trick;
 
 public class CcreateTrick {
     public static Response createTrick(Trick trick){
-        if(validTrick(trick)) {
-            return Response.status(404).build();
-        };
         trick.persist(); 
         return Response.ok(trick).build();
-    }
-
-    public static boolean validTrick(Trick trick) {
-        return !(   trick.getName() == null || 
-                    trick.getDifficult() < 0 || 
-                    trick.getDescription() == null || 
-                    trick.getNumberOfBalls() < 0);
     }
 }
