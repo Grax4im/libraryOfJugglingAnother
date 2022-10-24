e67e47978b325624a87a893dc5a31fbc4f9f8015
rob <rob@rob.com>
2022-10-24 19:47:13 -0300

    search by id

--- src/main/java/ifrs/edu/View/Read.java
+++ src/main/java/ifrs/edu/View/Read.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import ifrs.edu.Entity.Trick;
 import ifrs.edu.Entity.User;
@@ -17,6 +18,24 @@ public class Read {
     @Path("trick")
     public List<PanacheEntityBase> seeAllTricks() {
         return Trick.listAll();
+    }
+
+    @GET
+    @Path("trick/{id}")
+    public Trick seeTrickById(@PathParam("id") Long id){
+        return Trick.findById(id);
+    }
+
+    @GET
+    @Path("trick/{difficult}")
+    public List<PanacheEntityBase> seeTrickByDifficult(@PathParam("difficult") Long difficult){
+        return Trick.find("difficult", difficult);
+    }
+
+    @GET
+    @Path("trick/{balls}")
+    public List<PanacheEntityBase> seeTrickByBalls(@PathParam("balls") Long balls){
+        return Trick.find("numberOfBalls", balls);
     }
 
     @GET
